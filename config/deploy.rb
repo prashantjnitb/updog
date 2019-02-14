d601f82de0c5f8a5f928a019b2c25932d5c05d87
Prashant <prashantjnitb@gmail.com>
2019-02-14 18:53:58 +0530

    Quick changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,5 @@ set :repo_url, "git@github.com:prashantjnitb/updog.git"
 
 set :deploy_to, '/home/deploy/updog'
 
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml", "tmp/pids/request-count.txt"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml", "tmp/request-count.txt"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "certs"
