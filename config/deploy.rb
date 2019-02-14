93af225a2dfbfe283d75e0bf0183696342f31e5d
Prashant <prashantjnitb@gmail.com>
2019-02-14 18:45:19 +0530

    Deployment script fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "updog"
+set :repo_url, "git@github.com:prashantjnitb/updog.git"
 
 set :deploy_to, '/home/deploy/updog'
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml", "tmp/pids/request-count.txt"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "certs"
